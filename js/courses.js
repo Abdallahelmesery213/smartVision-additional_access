e8f942a796d29cd12a7593ba92e3a6c536e906b3
Abdallah Elmesery <abdallahelmesery@gmail.com>
2021-11-01 15:28:27 +0200

    additional access1

---
+++ js/courses.js
@@ -0,0 +1,29 @@
+// swiper in Courses
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+
+
+
+  // for scrol top
+  function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  //For Adding Rate
+  var rateOverlay = document.querySelector(".rate-overlay");
+  var closeBtn = document.getElementById("close");
+  var addRateBtn = document.getElementById("add-rate");
+  addRateBtn.addEventListener("click",function(){
+    rateOverlay.classList.toggle("show-overlay");
+  });
+  closeBtn.addEventListener("click", function(){
+    rateOverlay.classList.toggle("show-overlay");
+  })
+  
---
+++ js/script.js
@@ -0,0 +1,54 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper = new Swiper(".mySwiper6", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // swipper in who we ?
+  var swiper = new Swiper(".mySwiperWh", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
+
+  // $(document).ready(function(){
+  //   $(".icon-footer").click(function(){
+  //     $(".arr-footer").slideToggle("slow");
+  //   });
+  // });
+
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+var activeButt = document.getElementById("btnn");
+activeButt.addEventListener("click" , function(){
+
+  activeButt.classList.toggle("removeBG");
+})
+
