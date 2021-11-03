33a7a2f2c4eadb7951a3a81c330e2eba457021e1
Abdallah Elmesery <abdallahelmesery@gmail.com>
2021-11-03 17:06:37 +0200

    update-profile-page

--- js/changeName.js
+++ js/changeName.js
@@ -11,7 +11,7 @@ changeNameBtn.addEventListener("click", function(){
 });
 closeBtn.addEventListener("click", function(){
     overlayProfile.classList.toggle("show-change");
-})
+});
 
 // for changing Email
 var changeEmailBtn = document.getElementById("changeEmail");
@@ -22,15 +22,54 @@ changeEmailBtn.addEventListener("click", function(){
 });
 closeEmailBtn.addEventListener("click" , function(){
     overlayEmail.classList.toggle("show-change");
-})
+});
 
-// // for changing Phone
-// var changePhoneBtn = document.getElementById("changePhone");
-// var overlayPhone = document.getElementById("overlay-Phone");
-// var closePhoneBtn = document.getElementById("closePhone");
-// changePhoneBtn.addEventListener("click", function(){
-//     overlayPhone.classList.toggle("show-change");
-// });
-// closePhoneBtn.addEventListener("click" , function(){
-//     overlayPhone.classList.toggle("show-change");
-// })
+// for changing Phone
+var changePhoneBtn = document.getElementById("changePhone");
+var overlayPhone = document.getElementById("overlay-phone");
+var closePhoneBtn = document.getElementById("closePhone");
+changePhoneBtn.addEventListener("click", function(){
+    console.log("phone");
+    overlayPhone.classList.toggle("show-change");
+});
+closePhoneBtn.addEventListener("click" , function(){
+    console.log("close phone");
+    overlayPhone.classList.toggle("show-change");
+});
+
+// for changing pass
+var changePassBtn = document.getElementById("changePass");
+var overlayPass = document.getElementById("overlay-pass");
+var closePassBtn = document.getElementById("closePass");
+changePassBtn.addEventListener("click", function(){
+    overlayPass.classList.toggle("show-change");
+});
+closePassBtn.addEventListener("click" , function(){
+    overlayPass.classList.toggle("show-change");
+});
+
+// for checkPass
+function myFunction() {
+    var x = document.getElementById("pass1");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+function myFunction2() {
+    var x = document.getElementById("pass2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+function myFunction3() {
+    var x = document.getElementById("pass3");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
